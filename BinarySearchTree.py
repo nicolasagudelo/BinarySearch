dfbc16361a450aed3f46ad244a2888c3f28dd8fb
Nicolas Agudelo <nicoalbert95@hotmail.com>
2022-08-17 12:30:39 +0000

    Added inorder, preorder, and postorder traversal for the tree. Added delete_node method to remove nodes from the tree.

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -29,15 +29,90 @@ class BinarySearchTree:
 
         return 'Value not found'
 
-root = BinarySearchTree(100)
-root.insert(50)
-root.insert(125)
-root.insert(75)
-root.insert(25)
+    def inorder_traversal(self):
+        if self.left:
+            self.left.inorder_traversal()
+        print(f'Depth = {self.depth}, Value = {self.value}')
+        if self.right:
+            self.right.inorder_traversal()
+    
+    def preorder_traversal(self):
+        print(f'Depth = {self.depth}, Value = {self.value}')
+        if self.left:
+            self.left.preorder_traversal()
+        if self.right:
+            self.right.preorder_traversal()
 
-print('\t\t',root.value,'\n')
-print('\t',root.left.value, '\t\t', root.right.value)
-print(root.left.left.value, '\t\t',root.left.right.value)
+    def postorder_traversal(self):
+        if self.left:
+            self.left.postorder_traversal()
+        if self.right:
+            self.right.postorder_traversal()
+        print(f'Depth = {self.depth}, Value = {self.value}')
 
-print(root.get_node_by_value(125).value)
-print(root.get_node_by_value(124))
+    def minValueNode(self):
+        while(self.left is not None):
+            self = self.left
+  
+        return self
+
+    def deleteNode(self, value):
+
+        if self is None:
+            return self
+    
+        if value < self.value:
+            self.left = self.left.deleteNode(value)
+    
+        elif(value > self.value):
+            self.right = self.right(value)
+    
+        else:
+    
+            if self.left is None:
+                return self.right
+    
+            elif self.right is None:
+                return self.left
+                
+            temp = self.right.minValueNode()
+    
+            # Copy the inorder successor's 
+            # content to this node
+            self.value = temp.value
+    
+            # Delete the inorder successor
+            self.right = self.right.deleteNode(temp.value)
+    
+        return self
+            
+
+
+self = BinarySearchTree(100)
+self.insert(50)
+self.insert(125)
+self.insert(75)
+self.insert(25)
+
+print('\t\t',self.value,'\n')
+print('\t',self.left.value, '\t\t', self.right.value)
+print(self.left.left.value, '\t\t',self.left.right.value)
+
+print(self.get_node_by_value(125).value)
+print(self.get_node_by_value(124))
+
+print('INORDER:')
+self.inorder_traversal()
+print('PREORDER:')
+self.preorder_traversal()
+print('POSTORDER:')
+self.postorder_traversal()
+
+self.deleteNode(50)
+
+print('INORDER AFTER DELETE:')
+self.inorder_traversal()
+print('PREORDER AFTER DELETE:')
+self.preorder_traversal()
+print('POSTORDER AFTER DELETE:')
+self.postorder_traversal()
